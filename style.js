2ef5acaadf081540ddfd0949aa0bc4d5e948f253
cft-party <135412335+cft-party@users.noreply.github.com>
2024-06-03 18:41:10 +0700

    Add files via upload

--- style.js
+++ style.js
@@ -1,6 +1,6 @@
 const validCredentials = [
-{ memberId: "cftp-ms-0167", password: "12345678" },
-{ memberId: "ipstudio", password: "Dara12369874" },
-{ memberId: "poohdi", password: "poohdi" }
-             // Add more credentials as neede
+    { memberId: "cftp-ms-0167", password: "12345678" },
+    { memberId: "ipstudio", password: "Dara12369874" },
+    { memberId: "poohdi", password: "poohdi" }
+                 // Add more credentials as neede
 ];
