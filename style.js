50d4cfbb6dcb6c89a96cc25893640ec7d3f46f03
cft-party <135412335+cft-party@users.noreply.github.com>
2024-06-03 18:01:32 +0700

    Create style.js

---
+++ style.js
@@ -0,0 +1,7 @@
+// List of valid credentials
+const validCredentials = [
+    { memberId: "cftp-ms-0167", password: "12345678" },
+    { memberId: "cftp-ms-0267", password: "12345678" },
+    { memberId: "cftp-ms-0367", password: "12345678" }
+    // Add more credentials as needed
+];
