e53ca74f1f0027086de225b6ea7bdb46270d6b65
cft-party <135412335+cft-party@users.noreply.github.com>
2024-06-03 18:22:26 +0700

    Update style.js

--- style.js
+++ style.js
@@ -1,7 +1,7 @@
 // List of valid credentials
 const validCredentials = [
     { memberId: "cftp-ms-0167", password: "12345678" },
-    { memberId: "cftp-ms-0267", password: "12345678" },
-    { memberId: "cftp-ms-0367", password: "12345678" }
+    { memberId: "ipstudio", password: "Dara12369874" },
+    { memberId: "poohdi", password: "poohdi" }
     // Add more credentials as needed
 ];
