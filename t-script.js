8f46aeb9e493861647ac6778d84197cd6cfac31a
cft-party <135412335+cft-party@users.noreply.github.com>
2025-01-28 20:10:45 +0700

    Create t-script.js

---
+++ t-script.js
@@ -0,0 +1,6 @@
+// Loader Script
+window.addEventListener('load', function () {
+  const loader = document.querySelector('.loader');
+  // ซ่อน Loader หลังจากโหลดหน้าเว็บเสร็จ
+  loader.classList.add('hidden');
+});
